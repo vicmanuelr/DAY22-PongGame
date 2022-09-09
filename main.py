8523483576e19a0b8a7c26af8ffecf1a4feb469e
Victor Ponce <vicmanuelr@gmail.com>
2022-09-08 18:30:07 -0600

    paddle.py with Paddle class created for movement of paddle

--- main.py
+++ main.py
@@ -1,31 +1,21 @@
 from turtle import Screen, Turtle
-UP = 90
-DOWN = 270
+from paddle import Paddle
 
+WIDTH = 800
+HEIGHT = 600
+RIGHT_PADDLE_COOR = (350, 0)
+LEFT_PADLE_COOR = (-350, 0)
 
 screen = Screen()
+right_paddle = Paddle(RIGHT_PADDLE_COOR)
+
+
 screen.setup(width=800, height=600, startx=100, starty=-75)
 screen.bgcolor("black")
 screen.title("Pong Game")
 screen.listen()
-screen.onkey(fun=snake.up, key="Up")
-screen.onkey(fun=snake.down, key="Down")
-screen.onkey(fun=snake.left, key="Left")
-screen.onkey(fun=snake.right, key="Right")
-
-
-right_paddle = Turtle("square")
-right_paddle.penup()
-right_paddle.speed("slow")
-right_paddle.color("white")
-right_paddle.setheading(90)
-right_paddle.shapesize(stretch_wid=1, stretch_len=5)
-right_paddle.goto(350, 0)
-
-
-right_paddle.forward(20)
-
-
+screen.onkey(fun=right_paddle.up, key="Up")
+screen.onkey(fun=right_paddle.down, key="Down")
 
 
 screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+
+
+class Paddle:
+
+    def __init__(self, coordinates):
+        self.paddle = Turtle("square")
+        self.paddle.speed("fastest")
+        self.paddle.penup()
+        self.paddle.color("white")
+        self.paddle.setheading(UP)
+        self.paddle.shapesize(stretch_wid=1, stretch_len=5)
+        self.paddle.goto(coordinates)
+
+    def up(self):
+        self.paddle.forward(20)
+
+    def down(self):
+        self.paddle.backward(20)
+
+    def collision_wall(self):
+        if self.head.xcor() > 290 or self.head.xcor() < -300:
+            return True
+        elif self.head.ycor() > 290 or self.head.ycor() < -300:
+            return True
